c6716d31e5fb7ff369bb1a0376d14d64605ed0c7
Jia Yi Li <lijiay@oregonstate.edu>
2020-12-06 03:17:06 -0800

    adding updated no-loss lottery contract and binary betting contract

---
+++ Polaris_Lottery/contracts/BinaryLottery.sol
@@ -0,0 +1,187 @@
+pragma solidity ^0.7.1;
+/**************************************************************************
+ * *Program: BinaryLottery.sol
+ * *Author: Polaris Lottery Group
+ * *Version: 1.1
+ * *Date: 12/02/2020
+ * *Description: A contract that allows players to place a bet in one of 
+ two choices given. 
+ 
+ * *Note: 0.01 eth = 1 stars
+ **************************************************************************/
+import "./StarsCoins.sol";
+import "./SponsorWhitelistControl.sol";
+import "./Star.sol";
+/*
+NBA: 1 = Home Team, 2 =  Visitor Team
+ETH: 1 = True, 2 = False
+*/
+
+contract BinaryLottery {
+    //declare required variables
+    address public manager;
+
+    address payable[] public players1;
+    address payable[] public players2;
+    mapping(address payable => uint) public bets;
+
+    uint score1;
+    uint score2;
+
+    uint teamChosen;
+    uint winningTeam;
+
+    uint lotteryId;
+    bool LotteryOpen;
+    
+    //Sponsorship
+    address swc_addr = 0x0888000000000000000000000000000000000001; //swc address
+    SponsorWhitelistControl swc = SponsorWhitelistControl(swc_addr);
+
+    event AnnounceWinner(uint _winningTeam, uint _id, uint _amount);
+
+    constructor(address payable _coinContract){
+        //play with stars coins
+        address payable coinContract = _coinContract; // contract of Stars 
+        StarsCoins stars = StarsCoins(coinContract);
+        
+        //initialize variables
+        manager = msg.sender;
+        lotteryId = 1;
+        LotteryOpen = false;
+    }
+    
+    //backend call this function to start lottery
+    function startLottery() private{
+        LotteryOpen = true;
+    }
+    
+    //frontend call this function to enter lottery
+    function enter(uint amount, uint teamChosen) public{
+        require(LotteryOpen == true, "Lottery: Lottery is not open.");
+        
+        //fixed bet
+        require(amount == 10, "Lottery: Incorrect number of Stars");
+        require(stars.balanceOf(msg.sender) >= amount, "Lottery: Insufficient balance of Stars");
+
+        //transfer stars to this contract with amount entered.
+        stars._fromTransfer(msg.sender, address(this), amount);
+
+        if (teamChosen == "1"){
+            players1.push(msg.sender);
+            bets[msg.sender] = amount;
+        }
+
+        if (teamChosen == "2"){
+            players2.push(msg.sender);
+            bets[msg.sender] = amount;
+        }
+    }
+
+    function updateScores(uint _score1, uint _score2) public view{
+        score1 = _score1;
+        score2 = _score2;
+    }
+    
+    function payWinner() private{
+        //close lottery
+        require(LotteryOpen == false, "Lottery: Lottery is open.");
+
+        //check winning team
+        if (winningTeam == 1){
+            //set winners to players1
+            address payable[]  winners = players1;
+            //calculate prize
+            uint amount = uint(players2.length * 10) / players1.length;
+        }
+        else{
+            //set winners to players2
+            address payable[]  winners = players2;
+            //calculate prize
+            uint amount = uint(players1.length * 10) / players2.length;
+        }
+
+        //loop thru winning players to pay them
+        for (uint i = 0; i < winners.length; i++){
+            //tranfer prize to winners
+            stars._fromTransfer(address(this), winners[index], amount);
+        }
+       
+        emit AnnounceWinner(winningTeam, lotteryId, amount);
+        
+    }
+
+    function resetLottery() private{
+        //close lottery
+        require(LotteryOpen == false, "Lottery: Lottery is open.");
+
+        //loop thru players1 to reset bets
+        for (uint i = 0; i < players1.length; i++){
+            bets1[players1[i]] = 0
+        }
+        //loop thru players2 to reset bets
+        for (uint i = 0; i < players2.length; i++){
+            bets2[players2[i]] = 0
+        }
+        
+        //reset arrays
+        players1 = new address payable[](0);
+        players2 = new address payable[](0);
+        lotteryId += 1;
+    }
+
+    //backend call to end lottery, pay back players and pick winner
+    function endLottery(uint _score1, uint _score2) private{
+        //close the lottery
+        LotteryOpen = false;
+
+        //update scores
+        updateScores(_score1, _score2);
+
+        //update winning team
+        if (score1 > score2){
+            winningTeam = 1;
+        }
+        else{
+            winningTeam = 2;
+        }
+
+        //pay the winning players
+        payWinner();
+        
+        //reset lottery setting for next round
+        resetLottery();
+
+    }
+
+    //helper functions:
+
+    function getplayers1() public view returns(address payable[] memory) {
+        return players1;
+    }
+
+    function getplayers2() public view returns(address payable[] memory) {
+        return players2;
+    }
+    
+    function getLotteryId() public view returns(uint) {
+        return lotteryId;
+    }
+
+    function getPool() public view returns(uint){
+        return stars.balanceOf(address(this));
+    }
+
+    function getLotteryOpen() public view returns(bool){
+        return LotteryOpen;
+    }
+
+    function addToWhitelist(address account) public {
+        require(msg.sender == manager, "Lottery: Only manager can access");
+        address[] memory a = new address[](1);
+        //a[0] = 0x0000000000000000000000000000000000000000; //all trans will be sponsored
+        a[0] = account; //sponsor specific user
+        swc.addPrivilege(a);
+    }
+
+}
--- Polaris_Lottery/contracts/Lottery.sol
+++ Polaris_Lottery/contracts/Lottery.sol
@@ -1,38 +1,53 @@
 pragma solidity ^0.7.1;
+/**************************************************************************
+ * *Program: BinaryLottery.sol
+ * *Author: Polaris Lottery Group
+ * *Version: 1.1
+ * *Date: 12/02/2020
+ * *Description: A no-loss lottery contract that executes after lottery 
+ window closes. The contract randomly selects one winner and pays earned 
+ interest amount to the winner, other players will be paid back their 
+ initial lottery bets.
 
-<<<<<<< HEAD
+ * *Note: 0.01 eth = 1 stars
+ **************************************************************************/
 import "./StarsCoins.sol";
 import "./SponsorWhitelistControl.sol";
-=======
 import "./Star.sol";
 
->>>>>>> c12575634bad1033c3832dc94ec243532e208efa
-/* goal: a contract that allow people to join and once a certain amount of people joined, 
-the contract randomly select a winner and pay to the winner 
-*/
-
 contract Lottery {
+    //declare required variables
     address public manager;
     address payable[] public players;
     uint lotteryId;
-    
-    //play with stars coins
-    address payable coinContract = 0x855D31D814Dce772976cc94D1f3E2b16e08fBB80; // contract of Stars 
-    StarsCoins stars = StarsCoins(coinContract);
+    bool LotteryOpen;
 
     //Sponsorship
     address swc_addr = 0x0888000000000000000000000000000000000001; //swc address
     SponsorWhitelistControl swc = SponsorWhitelistControl(swc_addr);
 
     event AnnounceWinner(address _winner, uint _id, uint _amount);
-    //event CheckData(address _user, uint256 _balance, uint256 amount);
 
-    constructor(){
+    constructor(){     
+        //play with stars coins
+        address payable coinContract = 0x855D31D814Dce772976cc94D1f3E2b16e08fBB80; // contract of Stars 
+        StarsCoins stars = StarsCoins(coinContract);
+
+        //initialize variables
         manager = msg.sender;
         lotteryId = 1;
+        LotteryOpen = false;
+    }
+    
+    //backend call this function to start lottery
+    function startLottery() private{
+        LotteryOpen = true;
     }
 
-    function enter(uint256 amount) public{        
+    //frontend call this function to enter lottery
+    function enter(uint amount) public{
+        require(LotteryOpen == true, "Lottery: Lottery is not open.");
+        
         //mapping 1 to 1 ether to stars: 0.01 eth = 1 stars
         require(amount == 10, "Lottery: Incorrect number of Stars");
         require(stars.balanceOf(msg.sender) >= amount, "Lottery: Insufficient balance of Stars");
@@ -41,52 +56,69 @@ contract Lottery {
         stars._fromTransfer(msg.sender, address(this), amount);
 
         players.push(msg.sender);
-        //check the number of players
-        if(players.length == 5){
-            pickWinner();
-        }
-
-        //emit CheckData(msg.sender, stars.balanceOf(msg.sender), amount);
-    }
-
-    function enterSponsored(address payable user, uint256 amount) public{
-        //mapping 1 to 1 ether to stars: 0.01 eth = 1 stars
-        require(amount == 10, "Lottery: Incorrect number of Stars");
-        require(stars.balanceOf(user) >= amount, "Stars: Insufficient balance");
-
-        //transfer stars to this contract with amount entered.
-        stars._fromTransfer(user, address(this), amount);
-
-        players.push(user);
-        //check the number of players
-        if(players.length == 5){
-            pickWinner();
-        }
+    
     }
 
     function random() private view returns (uint) {
         return uint(keccak256(abi.encodePacked(block.timestamp, players)));
     }
 
-    function pickWinner() private{
+    function pickWinner(uint prize) private{
+        require(LotteryOpen == false, "Lottery: Lottery is open.");
+
+        //get random number
         uint index = random() % players.length;
-        
-        //award winner with stars coins
-        uint256 amount = stars.balanceOf(address(this));
-        emit AnnounceWinner(players[index], lotteryId, amount);
+
         //tranfer balance to winner
-        stars._fromTransfer(address(this), players[index], amount);
+        stars._fromTransfer(address(this), players[index], prize);
+
+        emit AnnounceWinner(players[index], lotteryId, prize);
+    }
+    
+    function paybackPlayers() private{
+        require(LotteryOpen == false, "Lottery: Lottery is open.");
         
+        //lottery bets
+        uint256 amount = 10;
+        //tranfer amount back to players
+        for (uint i = 0; i < players.length; i++){
+            stars._fromTransfer(address(this), players[i], amount);
+        }
+    }
+
+    //backend call to end lottery, pay back players and pick winner
+    function endLottery(uint prize) private{
+        //close the lottery
+        LotteryOpen = false;
+
+        //pays back players initial bets
+        paybackPlayers();
+
+        //pick the winner
+        pickWinner(prize);
+        
+        //reset players array
         players = new address payable[](0);
+        
         lotteryId += 1;
     }
+
+    //helper functions:
 
     function getPlayers() public view returns(address payable[] memory) {
         return players;
     }
+    
+    function getLotteryId() public view returns(uint) {
+        return lotteryId;
+    }
 
     function getPool() public view returns(uint){
         return stars.balanceOf(address(this));
+    }
+
+    function getLotteryOpen() public view returns(bool){
+        return LotteryOpen;
     }
 
     function addToWhitelist(address account) public {
@@ -96,5 +128,20 @@ contract Lottery {
         a[0] = account; //sponsor specific user
         swc.addPrivilege(a);
     }
+
+    //function enterSponsored(address payable user, uint256 amount) public{
+        //mapping 1 to 1 ether to stars: 0.01 eth = 1 stars
+      //  require(amount == 10, "Lottery: Incorrect number of Stars");
+      //  require(stars.balanceOf(user) >= amount, "Stars: Insufficient balance");
+
+        //transfer stars to this contract with amount entered.
+      //  stars._fromTransfer(user, address(this), amount);
+
+      //  players.push(user);
+        //check the number of players
+      //  if(players.length == 5){
+     //       pickWinner();
+     //   }
+    //}
 
 }
