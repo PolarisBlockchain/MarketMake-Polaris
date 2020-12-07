53c5f98d87554146af8fab883716a5f10a238fc5
成镇宇 <cheng-zy17@mails.tsinghua.edu.cn>
2020-12-07 23:13:58 +0800

    feat: finished implementing conflux part, need testing

--- ExpressServer/index.js
+++ ExpressServer/index.js
@@ -1,3 +1,5 @@
+import { BinaryLottery_Address, BinaryLottery_ABI } from "./deployed_contract";
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const { Conflux } = require("js-conflux-sdk");
@@ -13,6 +15,25 @@ const NBA_BASE_URL = "https://api-nba-v1.p.rapidapi.com/";
 const BASE_COIN_PRICE_URL = "https://api.coingecko.com/api/v3" // documentation is here: https://www.coingecko.com/api/documentations/v3
 const axios = require('axios');
 
+const account = cfx.Account(PRIVATE_KEY); // create account instance
+console.log("Address: ", account.address); // 0x1bd9e9be525ab967e633bcdaeac8bd5723ed4d6b
+
+const conflux = new Conflux({
+  url: "http://testnet-jsonrpc.conflux-chain.org:12537",
+  defaultGasPrice: 100, // The default gas price of your following transactions
+  defaultGas: 1000000, // The default gas of your following transactions
+  logger: console,
+});
+
+const LotteryContract = {
+  name: 'Lottery',
+  abi: BinaryLottery_ABI,
+  contract: conflux.Contract({
+    abi: BinaryLottery_ABI,
+    address: BinaryLottery_Address,
+  }),
+}
+
 const COIN_LISTS = [
   { "id":"ethereum", "symbol":"eth", "name":"Ethereum" },
   { "id": "ethereum-classic", "symbol": "etc", "name": "Ethereum Classic" },
@@ -21,7 +42,8 @@ const COIN_LISTS = [
 
 const NBA_URL_PATHS = {
   seasons: "seasons",
-  seasonYear: "games/seasonYear/"
+  seasonYear: "games/seasonYear/",
+  gameDetails: "gameDetails/"
 }
 
 function getCoinPriceAxiosConfig(coinID) {
@@ -99,10 +121,37 @@ const main = () => {
   })
 
   /**
+   * @api {get} /api/v1/nba/timesup/:gameID End A NBA Game
+   * @apiName end-a-nba-game
+   * @apiParam {String} gameID  The ID of a game, fetched from `get-a-season` api.
+   * @apiSuccess (200)
+   */
+  app.post(BASE_URL + "/coins/timesup/:coinID", async (req, res) => {
+    const gameID = req.params.gameID;
+    let config = getNBAAPIAxiosConfig(NBA_URL_PATHS.gameDetails + gameID);
+    axios(config)
+    .then(function (response) {
+      const game = response.data.api.game[0];
+      let score1 = game.hTeam.score.points;
+      let score2 = game.vTeam.score.points
+
+      const txhash = await LotteryContract.contract.endLottery(score1, score2).sendTransaction({
+        from: account
+      });
+      console.log("txhash: ", txhash);
+
+      res.status(200);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  })
+
+  /**
    * @api {get} /api/v1/coins/list List coins
    * @apiName list-coins
    * @apiParam {Boolean} less  true if you want to just get the major coins info.
-   * @apiSuccess (200) {Number[]} years
+   * @apiSuccess (200) {} coins
    */
   app.get(BASE_URL + "/coins/list", async (req, res) => {
     let isLess = req.query.less
@@ -133,7 +182,7 @@ const main = () => {
    * @api {get} /api/v1/coins/:coinID Get Info of A Coin
    * @apiName get-info-of-a-coin
    * @apiParam {String} coinID  The ID of a coin, fetched from `list-coins` api.
-   * @apiSuccess (200) {Number[]} years
+   * @apiSuccess (200) {} coins
    */
   app.get(BASE_URL + "/coins/:coinID", async (req, res) => {
     const coinID = req.params.coinID;
@@ -155,46 +204,52 @@ const main = () => {
     });
   })
 
-  // Input: Contract Address
-  // need to check 
+  /**
+   * @api {get} /api/v1/coins/timesup/:coinID End A Coin Game
+   * @apiName end-a-coin-game
+   * @apiParam {String} coinID  The ID of a coin, fetched from `list-coins` api.
+   * @apiParam {Number} estimatedPrice  The Betting Price
+   * @apiParam {Boolean} teamOneIsHigher  if the team one is betting on higher, then its true, else its false.
+   * @apiSuccess (200)
+   */
   app.post(BASE_URL + "/coins/timesup/:coinID", async (req, res) => {
     const coinID = req.params.coinID;
+    const estimatedPrice = req.query.estimatedPrice;
+    const teamOneIsHigher = req.query.teamOneIsHigher;
     let config = getCoinPriceAxiosConfig(coinID);
     axios(config)
     .then(function (response) {
-      const data = response.data;
-      const output = {
-        "id": data.id,
-        "symbol": data.symbol,
-        "name": data.name,
-        "current_price": data.market_data.current_price.usd
+      const latestPrice = response.data.market_data.current_price.usd;
+
+      let score1 = 0, score2 = 0;
+      if (latestPrice > estimatedPrice) {
+        if (teamOneIsHigher) {
+          score1 = 1;
+          score2 = 0;
+        } else {
+          score1 = 0;
+          score2 = 1;
+        }
+      } else {
+        if (teamOneIsHigher) {
+          score1 = 0;
+          score2 = 1;
+        } else {
+          score1 = 1;
+          score2 = 0;
+        }
       }
 
-      //create conflux instance
-      const cfx = new Conflux({
-        url: "http://test.confluxrpc.org",
-        defaultGasPrice: 100,
-        defaultGas: 1000000,
-        // logger: console,
+      const txhash = await LotteryContract.contract.endLottery(score1, score2).sendTransaction({
+        from: account
       });
-
-      const account = cfx.Account(PRIVATE_KEY); // create account instance
-      console.log("Address: ", account.address); // 0x1bd9e9be525ab967e633bcdaeac8bd5723ed4d6b
-
-      // create contract instance
-      const contract = cfx.Contract({
-        abi: require("./contract/coin_abi.json"), //can be copied from remix
-        address: "0x8ac6bf0700ed41d1323a1f9c16d85d76f1196cdb",
-      });
-
-      // const receipt = await contract()
+      console.log("txhash: ", txhash);
 
       res.status(200);
     })
     .catch(function (error) {
       console.log(error);
     });
-
   })
 
   app.listen(port, () => console.log(`${port} is active`));
